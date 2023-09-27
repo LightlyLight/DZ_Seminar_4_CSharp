6abf6b0bf16b26b1fb94b469f002d825747400a0
Sveta <bdyysht@gmail.com>
2023-09-28 00:07:06 +0300

    initial commit

---
+++ Task34/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int GetNum(string message)
+{
+    Console.WriteLine($"Введите {message}: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int[] getRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(min, max);
+    }
+    return array;
+}
+
+int SumChet(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count = count + 1;
+
+        }
+    }
+    return count;
+}
+
+int SizeArr = GetNum("размер массива");
+
+int[] array = getRandomArray(SizeArr, 100, 999);
+int chet = SumChet(array);
+
+Console.WriteLine(string.Join(" // ", array));
+
+Console.WriteLine($"Количество чётных чисел в массиве = {chet}");
---
+++ TaskDopolnitelno/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+// int GetNum(string message)
+// {
+//     Console.WriteLine($"Введите {message}: ");
+//     int num = int.Parse(Console.ReadLine());
+//     return num;
+// }
+
+// int[] getRandomArray(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     Random rand = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = rand.Next(min, max);
+//     }
+//     return array;
+// }
+
+// int SizeArr = GetNum("размер массива");
+// int MinArr = GetNum("первый элемент массива");
+// int MaxArr = GetNum("последний элемент массива");
+
+// int[] array = getRandomArray(SizeArr, MinArr, MaxArr);
+
+// Console.WriteLine(string.Join(" // ", array));
